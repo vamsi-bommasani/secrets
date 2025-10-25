6d3281ff5dbb04011098b8a39a70b03401c7b42b
Bommasani Vamsi Krishna <vamsikrishnab1992@gmail.com>
2025-10-25 00:56:01 -0400

    initial commit of secrets +3

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,3 @@ output "kms_key_id" {
 output "kms_key_arn" {
   value = module.kms.key_arn
 }
-
-output "ssm_parameter_names" {
-  value = [for param in module.ssm_parameters : param.name]
-}
-
-output "ssm_parameter_arns" {
-  value = [for param in module.ssm_parameters : param.arn]
-}
